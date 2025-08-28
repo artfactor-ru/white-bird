48addd79c75a246bc50343fa71f5e78ea3e8cc57
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2025-08-28 15:22:27 +0300

    Закончил страницы услуг о компании специалистов сертификаты

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -249,6 +249,81 @@ if (document.querySelector(".stories__slider")) {
 	});
 }
 
+if (document.querySelector(".certificates__slider")) {
+	// Указываем скласс нужного слайдера
+	// Создаем слайдер
+	new Swiper(".certificates__slider", {
+		// Указываем скласс нужного слайдера
+		// Подключаем модули слайдера
+		// для конкретного случая
+		modules: [Navigation],
+		observer: true,
+		observeParents: true,
+		slidesPerView: 3,
+		spaceBetween: 25,
+		autoHeight: true,
+		speed: 800,
+
+		//touchRatio: 0,
+		//simulateTouch: false,
+		// loop: true,
+		//preloadImages: false,
+		//lazy: true,
+
+		// Эффекты
+		// effect: "fade",
+		// autoplay: {
+		// 	delay: 3000,
+		// 	disableOnInteraction: false,
+		// },
+
+		// Пагинация
+
+		// pagination: {
+		// 	el: ".swiper-pagination",
+		// 	clickable: true,
+		// },
+
+		// Скроллбар
+		/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+		// Кнопки "влево/вправо"
+		navigation: {
+			prevEl: ".certificates-prev",
+			nextEl: ".certificates-next",
+		},
+		/*
+			// Брейкпоинты
+			breakpoints: {
+				640: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+		// События
+		on: {},
+	});
+}
+
 if (document.querySelector(".contacts__slider")) {
 	const swiper = new Swiper(".contacts__slider", {
 		modules: [Navigation],
